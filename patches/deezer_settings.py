ccfa865766087c6f129f2a758d47c197ff3ba47a
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-03-28 20:31:44 +0530

    Update deezer_settings.py

--- patches/deezer_settings.py
+++ patches/deezer_settings.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
 stock_quality = "MP3_320"
-method_saves = ["0", "1", "2"]
+method_saves = ["0", "1", "2", "3"]
 
 qualities = {
+
+    "FLAC": {
+        "n_quality": "6",
+        "f_format": ".flac",
+        "s_quality": "745"
+    },
     "MP3_320": {
         "n_quality": "3",
         "f_format": ".mp3",
