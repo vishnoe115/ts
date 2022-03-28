ab622c2ce49df42c020382c0ce98f0487149df1d
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-03-28 20:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ API_ALBUM = API_URL + "/album/%s"
 API_SEARCH_TRK = API_URL + "/search/track/?q=%s"
 API_PLAYLIST = API_URL + "/playlist/%s"
 
-DEFAULT_QUALITY = "flac"
+DEFAULT_QUALITY = "FLAC"
 
 try:
     os.mkdir("tmp")
